3de31947e9909cb0154a29e076687c0d55556285
Jason Crane <jasoncraneuk@hotmail.com>
2015-11-05 15:04:09 +0000

    Added string parsing for incoming commands

--- ArduinoUniversalBluetoothRemote.ino
+++ ArduinoUniversalBluetoothRemote.ino
@@ -14,21 +14,61 @@
 #define txPin 11
 
 SoftwareSerial mySerial(rxPin, txPin); // RX / TX of the arduino
-char myChar ;
+char inBuffer[100];
+byte index = 0;
+
 void setup() {
   Serial.begin(9600);   
-  Serial.println("AT");
+  Serial.println("Arduino Bluetooth Universal Remote Server");
   mySerial.begin(9600);
+  mySerial.println("AT");
 
 }
+
 void loop() {
-  while (mySerial.available()) {
-    myChar = mySerial.read();
-    Serial.print(myChar);
+
+  //Incoming BT data stream
+  while (mySerial.available() >0) {
+   char aChar = mySerial.read();
+     if(aChar == ':')
+     {
+        processIncomingCommand();
+        
+        index = 0;
+        inBuffer[index] = NULL;
+     }
+     else
+     {
+        inBuffer[index] = aChar;
+        index++;
+        inBuffer[index] = '\0'; // Keep the string NULL terminated
+     }
   }
- while (Serial.available()) {
-    myChar = Serial.read();
+
+    
+  //Outbound BT stream 
+  while (Serial.available()) {
+    char myChar = Serial.read();
     Serial.print(myChar); //echo
     mySerial.print(myChar);
+  }
+}
+
+void processIncomingCommand() {
+  Serial.println("Command recieved:");
+  if (strstr(inBuffer, "ping") != 0) {
+    Serial.println("Ping");
+    
+  } else if (strstr(inBuffer, "record") != 0) {
+    Serial.println("Record");
+    
+  } else if (strstr(inBuffer, "getcode") != 0) {
+    Serial.println("Get Code");
+    
+  } else if (strstr(inBuffer, "sendcode") != 0) {
+    Serial.println("Send Code");
+    
+  } else {
+    Serial.println("Unknown command");
   }
 }
