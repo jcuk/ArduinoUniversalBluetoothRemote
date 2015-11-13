0d3b40a3093e1ce26ff3bda3aea3b9dcc6c1f906
Jason Crane <jasoncraneuk@hotmail.com>
2015-11-13 10:53:08 +0000

    Added license

--- ArduinoUniversalBluetoothRemote.ino
+++ ArduinoUniversalBluetoothRemote.ino
@@ -1,9 +1,30 @@
-//HM-10 bluetooth setup.
-// Pin connections from BT moule to Arduino:
-// VCC --> 5v
-// GND --> 0v
-// Rx --> Pin 11
-// Tx --> Pin 10
+/*
+    ArduinoUniversalBluetoothRemote.ino
+    
+    Bluetooth Arduino Universal IR Remote controller
+    Copyright (C)  2015 Jason Crane
+    jasoncraneuk (at) hotmail.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    
+Circuit - Pin connections from HM-10 BT moule to Arduino:
+* VCC --> 5v
+* GND --> 0v
+* Rx --> Pin 11
+* Tx --> Pin 10
+*/
 
 #include "remoteCore.h"
 #include <IRremote.h>
